1acf8f30fa06f4388463bf0584444c9f7de90a7a
Nauval Muhammad Firdaus <nauvalmfirdaus@gmail.com>
2020-05-31 13:23:06 +0700

    Update redblack_tree_gls2.cpp

--- binus/redblack_tree_gls2.cpp
+++ binus/redblack_tree_gls2.cpp
@@ -79,6 +79,9 @@ int main()
 {
     rbt *tree = make_rbt();
 
+    printf("Red Black Tree Simulation\n");
+    printf("=========================\n\n");
+	
     int input[] = {26, 10, 55, 1, 25, 30, 66, 44, 20, 73, 4, 99, 19};
     int i_size = sizeof(input) / sizeof(input[0]);
     for (int i = 0; i < i_size; i++) 
